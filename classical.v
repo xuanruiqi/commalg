0c3d05f7aaf0f1efe3cff84faa8b4914536dece1
Xuanrui Qi <me@xuanruiqi.com>
2020-10-05 10:33:05 +0900

    Use funeqE instead

--- classical.v
+++ classical.v
@@ -5,9 +5,3 @@ Require Export FunctionalExtensionality ProofIrrelevanceFacts ProofIrrelevance.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
-
-Lemma funext (A B : Type) (f g : A -> B) : f =1 g -> f = g.
-Proof.
-  move=> H. extensionality x. apply: H.
-Qed.
-
